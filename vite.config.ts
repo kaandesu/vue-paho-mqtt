75cd4846c3a2452aad03ae4a11ddb7eb93437ec6
kaan <kaandesu00@gmail.com>
2023-04-07 14:17:05 +0200

    feat: demo build config & npm script

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,30 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/pahoMqttPlugin/index.ts"),
-      name: "VuePahoMqtt",
-      // the proper extensions will be added
-      fileName: "vue-paho-mqtt",
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
+export default defineConfig(({ mode }) => {
+  if (mode === 'demo') {
+    return {
+      plugins: [vue()],
+    };
+  } else {
+    return {
+      plugins: [vue()],
+      build: {
+        lib: {
+          entry: resolve(__dirname, 'src/pahoMqttPlugin/index.ts'),
+          name: 'VuePahoMqtt',
+          fileName: 'vue-paho-mqtt',
+        },
+        rollupOptions: {
+          external: ['vue'],
+          output: {
+            globals: {
+              vue: 'Vue',
+            },
+          },
         },
       },
-    },
-  },
+    };
+  }
 });
